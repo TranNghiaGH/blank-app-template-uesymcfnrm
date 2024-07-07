443bbcc140d4affdc2465bab6f61aabe9e650405
TranNghiaGH <165242792+TranNghiaGH@users.noreply.github.com>
2024-07-08 02:03:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from st_login_form import login_form
+
+client = login_form()
+
+if st.session_state["authenticated"]:
+    if st.session_state["username"]:
+        st.success(f"Welcome {st.session_state['username']}")
+        ...
+
+    else:
+        st.success("Welcome guest")
+        ...
+else:
+    st.error("Not authenticated")
